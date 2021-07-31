2a1b7f3f545829bca3a670a53f6c3880ff0c9b13
Salami <salamisodikiolawale@gmail.com>
2021-07-31 18:17:28 +0200

    Last initialisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,23 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                'title' => 'Je suis une super question ?',
+                'content' => 'Guys ! je me demandais pourquoi les oranges étaient orange,
+                et non pas bleu ?',
+                'upvote' => 20,
+                'nbrOfResponse' => 15,
+                'author' => [
+                    'name' => 'Jean louis',
+                    'picture' => 'https://randomuser.me/api/portraits/women/81.jpg'
+                ],
+                'createdAt' => strtotime('yesterday')
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
